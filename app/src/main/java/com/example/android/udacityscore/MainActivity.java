d70fd9c9e004f2db52d9cd2062cadd7f089ec207
Vitaly Hlapkovsky <vitaly.hlapkovsky@gmail.com>
2017-11-20 23:05:07 +0200

    user can define teams names

--- app/src/main/java/com/example/android/udacityscore/MainActivity.java
+++ app/src/main/java/com/example/android/udacityscore/MainActivity.java
@@ -3,10 +3,15 @@ package com.example.android.udacityscore;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
-    //declare all variables
+
+    //declare global variables
     int scoreTeamA = 0;
     int scoreTeamB = 0;
     int undoActionA = 0;
@@ -18,8 +23,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         displayForTeamA(scoreTeamA);
         displayForTeamA(scoreTeamB);
+
+        // save the teams names
+        Button t1button = (Button) findViewById(R.id.team_a_save);
+        Button t2button = (Button) findViewById(R.id.team_b_save);
+
+        t1button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // find the text view for team A
+                EditText inputText = (EditText) findViewById(R.id.team_a_input);
+                String str = inputText.getText().toString();
+                TextView newText = (TextView) findViewById(R.id.team_a_name);
+                newText.setText( str);
+                //hide text view & button for Team A
+                EditText team_a_input = (EditText) findViewById(R.id.team_a_input);
+                team_a_input.setVisibility(View.GONE);
+                Button team_a_save = (Button) findViewById(R.id.team_a_save);
+                team_a_save.setVisibility(View.GONE);
+            }
+        });
+
+        t2button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //find the text view for team A
+                EditText inputText = (EditText) findViewById(R.id.team_b_input);
+                String str = inputText.getText().toString();
+                TextView newText = (TextView) findViewById(R.id.team_b_name);
+                newText.setText( str);
+                //hide text view & button for Team B
+                EditText team_b_input = (EditText) findViewById(R.id.team_b_input);
+                team_b_input.setVisibility(View.GONE);
+                Button team_b_save = (Button) findViewById(R.id.team_b_save);
+                team_b_save.setVisibility(View.GONE);
+            }
+        });
     }
-    //buttons on click for Team A
+    // buttons on click for the Team A
     public void plusThreeA(View view) {
         displayForTeamA(scoreTeamA = scoreTeamA + 3);
         undoActionA = 3;
@@ -38,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         undoActionB = 0;
     }
 
-    //buttons on click for Team B
+    // buttons on click for the Team B
     public void plusThreeB(View view) {
         displayForTeamB(scoreTeamB = scoreTeamB + 3);
         undoActionB = 3;
@@ -56,20 +95,27 @@ public class MainActivity extends AppCompatActivity {
         undoActionB = 1;
         undoActionA = 0;
     }
-    //reset button
+    // reset button
     public void Reset(View view) {
         displayForTeamA(scoreTeamA = 0);
         displayForTeamB(scoreTeamB = 0);
         undoActionB = 0;
         undoActionA = 0;
     }
-    //undo button
+    // undo button
     public void Undo(View view) {
         displayForTeamA(scoreTeamA = scoreTeamA - undoActionA);
         undoActionA = 0;
         displayForTeamB(scoreTeamB = scoreTeamB - undoActionB);
         undoActionB = 0;
     }
+
+    // hide the unneeded views
+    public void saveTeamA(View view) {
+        EditText team_a_input = (EditText) findViewById(R.id.team_a_input);
+        team_a_input.setVisibility(View.GONE);
+    }
+
     /**
      * Displays the given score for Team A.
      */
@@ -84,4 +130,6 @@ public class MainActivity extends AppCompatActivity {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(scoreTeamB));
     }
+
+
 }
